d8f8601ee683e7bda4f845df2cc082160dd5d829
Che B <cheb@ches-air.attlocal.net>
2016-11-13 22:03:27 -0500

    fixed the syntax

--- fcc.js
+++ fcc.js
@@ -1,10 +1,19 @@
-function reverseStr(str){
-  var a = str.split("");
-  console.log(a);
-  var re = a.reverse();
-  console.log(re);
-  var concat= re.join("");
-  console.log(concat);
+function reverseString(str) {
+  var spt = str.split("");
+  var re = spt.reverse();
+  var concat = re.join("");
+  return concat;
 }
 
-reverseStr("hello");
+reverseString("Greetings from Earth");
+
+// function reverseStr(str){
+//   var a = str.split("");
+//   console.log(a);
+//   var re = a.reverse();
+//   console.log(re);
+//   var concat= re.join("");
+//   console.log(concat);
+// }
+//
+// reverseStr("hello");
